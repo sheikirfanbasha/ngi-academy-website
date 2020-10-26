58864883114ae1bcaa5e7397df2124b32d38bda5
irfan basha <irfanbasha@IMCHLT134.local>
2020-10-26 08:46:15 +0530

    update courses listing view

--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -1,10 +1,15 @@
 <template>
     <div class="courses">
-        <b-list-group>
+        <b-button v-b-toggle.sidebar-1>Show Courses</b-button>
+        <b-sidebar id="sidebar-1" title="Courses List" shadow>
+        <div class="px-3 py-2">
+            <b-list-group>
             <b-list-group-item v-for="course in courses" :key="course.name" >
                 <router-link :to="{name: 'courseDetails', params: {id: course.id}}">{{course.name}}</router-link>
                 </b-list-group-item>
-        </b-list-group>
+            </b-list-group>
+        </div>
+        </b-sidebar>
         <router-view :key="$route.path" />
     </div>
 </template>
